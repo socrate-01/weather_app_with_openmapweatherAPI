3ba7fc97c193ec3cfb6f7d02c15c99c104689a23
Socratis <Seydinamouhamadousanoumbaye@esp.sn>
2022-08-28 09:31:28 -0300

    Add files via upload

--- lib/src/constants/app_colors.dart
+++ lib/src/constants/app_colors.dart
@@ -1,1 +1,10 @@
+import 'dart:ui';
 
+class AppColors {
+  static const rainBlueLight = Color(0xFF6EA1DE);
+  static const rainBlueDark = Color(0xFF364699);
+  static const rainGradient = [rainBlueLight, rainBlueDark];
+  static const accentColor = Color(0xFFe96e50);
+  static const searchColor = Color(0xff7c7c78);
+
+}
